97804b0744deb85a2ca0926064f36ed1231e0fb2
Daisuke-hub <general.daisuke@gmail.com>
2020-01-18 08:52:15 +0000

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "top" => "homes#top"
+
+  post "todolists" => "todolists#create"
+
+  get "todolist" => "todolists#index"
 end
